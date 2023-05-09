6d4d9aa0993f3ae7c97b99a12e49db39255ac6df
HansAndi <hansandiwijaya@gmail.com>
2023-05-09 10:53:27 +0700

    tampil foto galeri

--- resources/views/galeri/index.blade.php
+++ resources/views/galeri/index.blade.php
@@ -41,8 +41,9 @@
                                             <tr>
                                                 <td>{{ ($galeri->currentPage() - 1) * $galeri->perPage() + $key + 1 }}
                                                 </td>
-                                                <td id='gambar' class='gambar'>
-                                                    <img src="/assets/img/galeri/{{ $item->foto }}" alt="Foto">
+                                                <td>
+                                                    {{-- <img src="/assets/img/galeri/{{ $item->foto }}" alt="Foto"> --}}
+                                                    <img src="{{ asset('assets/img/galeri/'.$item->foto) }}" alt="" width="100px">
                                                 </td>
                                                 <td class="text-right">
                                                     <div class="d-flex justify-content-end">
