4357fa33994acd683aa2ea781d734dea428334b5
al <8c.alfan@gmail.com>
2023-05-24 05:14:44 +0700

    Minor update

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,11 +3,6 @@
     <section class="section">
         <div class="section-header">
             <h1>Dashboard</h1>
-            <div class="section-header-breadcrumb">
-                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-                <div class="breadcrumb-item"><a href="#">Components</a></div>
-                <div class="breadcrumb-item">Table</div>
-            </div>
         </div>
 
         <div class="row">
