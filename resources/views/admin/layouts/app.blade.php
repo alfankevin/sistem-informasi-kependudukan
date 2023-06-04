32ef0badef47905ae11675cfe8ac5fc3fc7eade9
al <8c.alfan@gmail.com>
2023-06-04 20:52:21 +0700

    Update icon

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>{{ config('app.name') }} </title>
+    <title>RW 5 Tanjungrejo</title>
+    <link rel="icon" href="/assets/img/malang.png">
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
