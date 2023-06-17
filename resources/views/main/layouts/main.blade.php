f97dea4be21406a4d555ec4af9f618067bc54db4
al <8c.alfan@gmail.com>
2023-06-17 19:11:32 +0700

    Minor update

--- resources/views/main/layouts/main.blade.php
+++ resources/views/main/layouts/main.blade.php
@@ -5,18 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>RW 5 Tanjungrejo</title>
         <link rel="icon" href="/assets/img/malang.png">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
+        <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/footers/">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-        <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/footers/">
-
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Cormorant:wght@300&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
         <link rel="stylesheet" href="/assets/css/main.css">
         <link rel="stylesheet" href="/assets/css/main/owl.theme.default.min.css">
--- resources/views/main/utils/footer.blade.php
+++ resources/views/main/utils/footer.blade.php
@@ -1,47 +1,57 @@
 <footer id="contact" class="bg-dark-blue text-white text-center text-lg-start">
-  <!-- Grid container -->
-  <div class="container p-4 ">
-    <!--Grid row-->
-    <div class="row pt-4 py-4">
-      <!--Grid column-->
-      <div class="col-lg-8 col-md-12 mb-4 mb-md-0">
+    <!-- Grid container -->
+    <div class="container p-4 ">
+        <!--Grid row-->
+        <div class="row pt-4 py-4">
+            <!--Grid column-->
+            <div class="col-lg-8 col-md-12 mb-4 mb-md-0">
+                <h5 class="text-uppercase fw-bold mb-3" style="font-family: 'Cormorant'">ERWE LIMO Tanjungrejo</h5>
+                <p>
+                    “Kecerdasan adalah kemampuan beradaptasi terhadap perubahan." - Stephen Hawking
+                </p>
+                <div class="flex row">
+                    <h6 class="text-uppercase mb-3 mt-4">Contact</h6>
+                    <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                        <p class="m-0" style="line-height: 0">
+                            <i class="fab fa-instagram me-3 text-white" style="color: #fdc800 !important"></i>
+                            <a href="https://instagram.com/rukunwarga05tanjungrejo/" target="_blank"
+                                class="text-white text-decoration-none">rukunwarga05tanjungrejo</a>
+                        </p>
+                    </div>
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <p class="m-0" style="line-height: 0">
+                            <i class="far fa-paper-plane me-3 text-white" style="color: #fdc800 !important"></i>
+                            <a href="https://wa.me/628111615533?text='Permisi, saya mau tanya :)'" target="_blank"
+                                class="text-white text-decoration-none">Contact Person</a>
+                        </p>
+                    </div>
+                    <div class="col-lg-5 col-md-6 mb-4 mb-md-0">
+                        <p class="m-0" style="line-height: 0">
+                            <i class="far fa-envelope me-3 text-white" style="color: #fdc800 !important"></i>
+                            <a href="mailto:rw05tanjungrejo@gmail.com"
+                                class="text-white text-decoration-none">rw05tanjungrejo@gmail.com</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!--Grid column-->
 
-        <h5 class="text-uppercase">ERWE LIMO Tanjungrejo</h5>
-        <p>
-          “Kecerdasan adalah kemampuan beradaptasi terhadap perubahan." - Stephen Hawking
-        </p>
-        <div class="flex row">
-          <h6 class="text-uppercase fw-bold mb-4 mt-3">Contact</h6>
-          <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
-              <p>
-                <i class="fab fa-instagram me-3 text-white" style="color: blue"></i>
-                <a href="https://instagram.com/rukunwarga05tanjungrejo/" target="_blank" style="text-decoration: none;" class="text-white">rukunwarga05tanjungrejo</a></p>
-          </div>
-          <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-              <p><i class="fas fa-phone me-3 text-white"></i><a href="https://wa.me/628111615533?text='Permisi, saya mau tanya :)'" target="_blank" style="text-decoration: none;" class="text-white">Contact Person</a></p>
-          </div>
-          <div class="col-lg-5 col-md-6 mb-4 mb-md-0">
-              <p><i class="fas fa-envelope me-3 text-white"></i><a href="mailto:rw05tanjungrejo@gmail.com" style="text-decoration: none;" class="text-white">rw05tanjungrejo@gmail.com</a></p>
-          </div>
+            <!--Grid column-->
+            <div class="col-lg-4 col-md-6 mb-6 mb-md-0">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7902.203939200117!2d112.61045780732552!3d-7.988405393243297!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78837616f5f4b7%3A0xc258114edc305fd3!2sBalai%20RW%205%20Kelurahan%20Sukun%20Malang!5e0!3m2!1sid!2sid!4v1683680772912!5m2!1sid!2sid"
+                    class="w-100 h-100 border-0" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <!--Grid column-->
         </div>
-
-      </div>
-      <!--Grid column-->
-
-      <!--Grid column-->
-      <div class="col-lg-4 col-md-6 mb-6 mb-md-0">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7902.203939200117!2d112.61045780732552!3d-7.988405393243297!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78837616f5f4b7%3A0xc258114edc305fd3!2sBalai%20RW%205%20Kelurahan%20Sukun%20Malang!5e0!3m2!1sid!2sid!4v1683680772912!5m2!1sid!2sid" class="w-100 h-100" style="border:0;" allowfullscreen="" loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"></iframe>
-      </div>
-      <!--Grid column-->
+        <!--Grid row-->
     </div>
-    <!--Grid row-->
-  </div>
-  <!-- Grid container -->
+    <!-- Grid container -->
 
-  <!-- Copyright -->
-  <div class="copyright text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © Copyright 2023. 2D Tanjung Anom.
-   </div>
-  <!-- Copyright -->
+    <!-- Copyright -->
+    <div class="copyright text-center p-3">
+        © Copyright 2023. 2D Tanjung Anom.
+    </div>
+    <!-- Copyright -->
 </footer>
