770aacdf75059c7723fe0db9bdf0e3aee7cc5643
Salwalabibah <salwalabibah04@gmail.com>
2023-06-23 07:53:41 +0700

    menambahkan judul potensi home

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -145,10 +145,19 @@
     {{-- Potensi --}}
     <div class="potensi album" id="potensi" style="padding-top: 40px">
         <div class="container">
-            <div class="pb-5 text-center">
-                <h2 class="text-center m-0">Potensi UMKM</h2>
+            <div class="row justify-content-between pb-5 mx-1">
+                <div class="col-4 w-50">
+                  <h2 class="text-left m-0">Potensi UMKM</h2>
+                </div>
+                <div class="col-4 d-flex justify-content-end w-50">
+                  <a href="/potensi" class="see-more fw-bold mt-2" style="text-decoration: none; font-size: 13pt" >Lihat lainnya
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+                      </svg>
+                </a>
+                </div>
             </div>
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4 mx-auto">
+            <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3 g-4 mx-auto">
                 @foreach ($potensi as $item)
                 <div class="col mb-4">
                     <div class="">
@@ -161,10 +170,10 @@
                         </div>
                         <div>
                             <div class="row justify-content-center align-content-center">
-                                <div class="col pt-3">
+                                <div class="col pt-3 w-50">
                                     <span class="font-weight-bold">Rp 25000</span>
                                 </div>
-                                <div class="col">
+                                <div class="col d-flex justify-content-end w-50">
                                     <a href="{{ $item->sosial_media }}" class="text-decoration-none btn btn-buy m-3" id="custom-text-link">beli sekarang</a>
                                 </div>
                             </div>
