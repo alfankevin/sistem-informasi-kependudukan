af1f5daa9c22e8acd81610357253e781a0585445
alfankevin <8c.alfan@gmail.com>
2023-07-14 16:33:31 +0700

    Fix reset-password blade

--- resources/views/admin/auth/reset-password.blade.php
+++ resources/views/admin/auth/reset-password.blade.php
@@ -27,17 +27,15 @@
                     <div
                         class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                         <div class="login-brand">
-                            <img src="../assets/img/stisla-fill.svg" alt="logo" width="100"
-                                class="shadow-light rounded-circle">
+                            <img src="../assets/img/malang.png" alt="logo" width="100">
                         </div>
 
-                        <div class="card card-primary">
+                        <div class="card card-info">
                             <div class="card-header">
-                                <h4>Reset Password</h4>
+                                <h4 class="text-info">Reset Password</h4>
                             </div>
 
                             <div class="card-body">
-                                <p class="text-muted">We will send a link to reset your password</p>
                                 @if (session('status'))
                                     <div class="alert alert-success" role="alert">
                                         {{ session('status') }}
@@ -83,7 +81,7 @@
                                     </div>
 
                                     <div class="form-group">
-                                        <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                        <button type="submit" class="btn btn-info btn-lg btn-block" tabindex="4">
                                             Reset Password
                                         </button>
                                     </div>
